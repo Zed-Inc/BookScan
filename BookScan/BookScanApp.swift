d85d231468822ea3cabf979b11ebab87e229b34d
Zoran-Bailey <s3775574@student.rmit.edu.au>
2021-01-05 12:32:59 +1100

    Initial Commit

---
+++ BookScan/BookScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookScanApp.swift
+//  BookScan
+//
+//  Created by Zed on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BookScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
